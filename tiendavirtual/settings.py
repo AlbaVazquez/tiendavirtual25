bc1fb4b90c4e5f67261f813f1b94e9c183f6cd11
AlbaVazquez <xavazgui498@ieshnosmachado.org>
2026-01-20 08:56:39 +0100

    creado y conectado a base de datos

--- tiendavirtual/settings.py
+++ tiendavirtual/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'tiendavirtual.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tiendavirtual25_db',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
